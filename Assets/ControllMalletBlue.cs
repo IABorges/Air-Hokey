03b6675475e739b2a2019d0ce069712bdf98a5e9
igordearaujoborges67@gmail.com <igordearaujoborges67@gmail.com>
2026-08-21 22:04:30 -0300

    comiit inicial

---
+++ Assets/ControllMalletBlue.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ControllMalletBlue : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var pos = transform.position;
+        pos.x = mousePos.x;
+        pos.y = mousePos.y;
+        transform.position = pos;
+
+    }
+}
